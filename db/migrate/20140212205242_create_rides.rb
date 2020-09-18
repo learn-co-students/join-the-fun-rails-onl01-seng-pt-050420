a6af2b540193253f753b55ff5bbeba841fcba30a
ameerrah9 <mccullers.ghameerah@gmail.com>
2020-09-18 14:19:27 -0400

    Done.

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -1,8 +1,8 @@
 class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
-      t.integer "passenger_id", null: false
-      t.integer "taxi_id",      null: false
+      t.integer "passenger_id"
+      t.integer "taxi_id"
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200918170459) do
+ActiveRecord::Schema.define(version: 20140212205318) do
 
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "passengers_taxis", id: false, force: :cascade do |t|
-    t.integer "passenger_id", null: false
-    t.integer "taxi_id",      null: false
-    t.index ["passenger_id", "taxi_id"], name: "index_passengers_taxis_on_passenger_id_and_taxi_id"
-    t.index ["taxi_id", "passenger_id"], name: "index_passengers_taxis_on_taxi_id_and_passenger_id"
-  end
-
   create_table "rides", force: :cascade do |t|
-    t.integer  "passenger_id", null: false
-    t.integer  "taxi_id",      null: false
+    t.integer  "passenger_id"
+    t.integer  "taxi_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
