47c69499a963a6d85b41871484665a4bedaeff65
ryancdavey1 <ryancdavey1@gmail.com>
2020-09-22 23:31:06 -0400

    Done.

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,4 +1,7 @@
 class Passenger < ActiveRecord::Base
-  has_many :passengers_taxis
-  has_many :taxis, through: :passengers_taxis
+  # has_many :rides
+  # has_many :passengers_taxis
+  # has_many :taxis, through: :passengers_taxis
+  has_many :rides
+  has_many :taxis, through: :rides
 end
